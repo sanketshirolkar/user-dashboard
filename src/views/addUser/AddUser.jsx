c773ec5db6624d152108e8d818e5ad8fc090531c
Sanket Shirolkar <snktshirolkar@gmail.com>
2022-09-12 05:13:28 +0530

    added form validations

--- src/views/addUser/AddUser.jsx
+++ src/views/addUser/AddUser.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addUser.css";
 import Input from "../../components/input/Input";
 import { useSelector, useDispatch } from "react-redux";
@@ -15,6 +15,10 @@ const AddUser = () => {
     allUsersList;
   const counterValue = useSelector(getCount);
 
+  // const [formErrors, setFormErrors] = useState({});
+  // const [isSubmit, setIsSubmit] = useState(false);
+  console.log(newUser?.name);
+
   const handleChange = (name, e) => {
     let newUserObject = { ...allUsersList };
     newUser[counterValue] = {
@@ -29,6 +33,7 @@ const AddUser = () => {
   };
 
   const handleSubmit = () => {
+    // setFormErrors(validate(newUser[0]));
     let newUserObject = { ...allUsersList };
     newUserObject = Object.assign({}, newUserObject, {
       newUser,
@@ -36,10 +41,36 @@ const AddUser = () => {
     });
     dispatch(storeNewUser(newUserObject));
     dispatch(increment());
+    alert("User Added Successfully");
   };
+
+  // const validate = (values) => {
+  //   const errors = {};
+  //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  //   if (!values.name) {
+  //     errors.name = "Name is required!";
+  //   }
+  //   if (!values.email) {
+  //     errors.email = "Email is required!";
+  //   } else if (!regex.test(values.email)) {
+  //     errors.email = "This is not a valid email format!";
+  //   }
+  //   if (!values.address) {
+  //     errors.address = "Address is required";
+  //   } else if (values.address.length < 4) {
+  //     errors.address = "Address must be more than 4 characters";
+  //   } else if (values.address.length > 20) {
+  //     errors.address = "Address cannot exceed more than 10 characters";
+  //   }
+  //   return errors;
+  // };
 
   return (
     <div className="addUser_container">
+      {/* {Object.keys(formErrors).length === 0 && isSubmit && (
+        <div className="ui message success">Signed in successfully</div>
+      )} */}
+
       <div className="input_container">
         <div className="form_container">
           <div className="title_container">Add User</div>
@@ -51,6 +82,7 @@ const AddUser = () => {
               value={newUser.name}
               onChange={(e) => handleChange("name", e)}
             />
+            {/* <p>{formErrors.name}</p> */}
             <Input
               type="email"
               name="email"
@@ -58,6 +90,7 @@ const AddUser = () => {
               value={newUser.email}
               onChange={(e) => handleChange("email", e)}
             />
+            {/* <p>{formErrors.email}</p> */}
             <Input
               type="text"
               name="address"
@@ -65,6 +98,7 @@ const AddUser = () => {
               value={newUser.address}
               onChange={(e) => handleChange("address", e)}
             />
+            {/* <p>{formErrors.address}</p> */}
           </div>
           <button className="button_container" onClick={handleSubmit}>
             Add User
